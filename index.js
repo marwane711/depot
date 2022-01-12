6d66a43dc119d22e35053a74a67ac2b862266d4f
Marwane Idrissi <marwane795@gmail.com>
2022-01-12 11:15:59 +0100

    nouvelle fonction getDay
    rebase

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 <<<<<<< HEAD
-let aPerson = { 
-    name: 'Jean',
-    age: 35,
-    gender: 'masculin',
-    interest: ['musique', 'Jeux videos'],
-};
+const operand = 12;
+const operand2 = 45;
 
-// on peut également déclarer et initialiser un objet de la façon suivante :
+let result = operand + operand2;
 
-let aPerson= new Object();
-aPerson.name = 'Jean';
-aPerson.age = 35;
-aPerson.gender = 'masculin';
-aPerson.interest = ['musique', 'badminton'];
->>>>>>> premiereBranche
+console.log("resultat de" + operand + "+" + operand2 + "=" + result);
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Dimanche";
+    break;
+  case 1:
+    day = "Lundi";
+    break;
+  case 2:
+     day = "Mardi";
+    break;
+  case 3:
+    day = "Mercredi";
+    break;
+  case 4:
+    day = "Jeudi";
+    break;
+  case 5:
+    day = "Vendredi";
+    break;
+  case 6:
+    day = "Samedi";
+}
+console.log("Aujourd'hui nous sommes " + day);
+>>>>>>> d1ebc58 (nouvelle fonction getDay)
